6810dc5a7c08e9a608bedb4e6ae4c274a689ac06
Fichdeveloper <33698787+Fichdeveloper@users.noreply.github.com>
2018-03-26 11:25:02 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,72 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System;
 using System.Text;
-
-namespace Homework_C
+using System.Threading.Tasks;
+using System.Collections.Generic;
+namespace 
 {
     class Program
-    {  
-        static void Main()
+    {
+        static void Main(string[] args)
         {
-            LinkedList<int> list = new LinkedList<int> { };
-
-            Console.WriteLine("Count: ");
+           
+            LinkedList<int> list = new LinkedList<int>();
+            Console.Write("Count= ");
             int count = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Remove number: ");
-            int k = Convert.ToInt32(Console.ReadLine());
+            Random random = new Random();
 
-            int remove_count = 0;
-
-            for (int i=1;i<=count;i++)
+            for (int i=0;i<count;i++)
             {
-                list.Add(i);
+                list.Add(random.Next(1, 10));      
             }
-
             list.tail.Next = list.head;
-
-                foreach (int i in list)
-                {
-                int remainder_count = 0;
-                    if (remove_count == k)
-                    {
-                       
-                        list.Remove(i);
-                        remove_count = 0;
-                        if (list.Count == 1)
-                        {                       
-                            break;
-                        }
-                    }
-
-                foreach (int remainder in list)
-                {
-                    Console.Write(" " + remainder);
-                    remainder_count++;
-                    if (remainder_count==list.Count)
-                    {
-                        break;
-                    }
-                }
-                Console.WriteLine();
-                remove_count++;
-                }
-
-            Console.WriteLine();
-
-            int n=0;
-
-            foreach (int last in list)
-            {
-                Console.WriteLine("Last node: " + last);
-                n++;
-                if (n == list.Count)
-                {
-                    break;
-                }
-            }
-
+            list.ShowList(list);
+           
         }
     }
 }
